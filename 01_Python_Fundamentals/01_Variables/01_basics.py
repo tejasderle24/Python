9f09c0b0af4ad548ab209b2049459d86a77a3ad5
tejasderle24 <tejasderle24@gmail.com>
2025-05-03 09:09:39 +0530

    Datatypes

---
+++ 01_Python_Fundamentals/01_Variables/01_basics.py
@@ -0,0 +1,21 @@
+# In Python, varibles are used to store data that can be used and manipulated throughout a program. A varible is created the moment you assign a value to it using the assignment oprater (=)
+
+
+name = "Tejas"      # String
+age = 22            # Integer
+cgpa = 8.36         # float
+
+"""
+# Rules of Defining a varible in Python
+    1. Variable name must start with a letter (a-z, A-Z) or an underscore(_).
+    2. Theycan conatin letters, number, and underscores.
+    3. Variables name are case-sensitive (age and Age are different).
+    4. Avoid using Pythn keywords (e.g. if, for, while) as Variable names.
+
+    for ex.
+        34age = 44 => Invalid because varible cannot start with a number.
+        age = 44   => Valid because varible can start with the alphbutes.
+        a$$ge = 76 => Invalid because varible cannot contant special charaters expcopt underscore (_)
+        _age = 44  => Its also valid because varible can start with the underscore.
+
+"""
---
+++ 01_Python_Fundamentals/01_Variables/02_data_types.py
@@ -0,0 +1,35 @@
+"""
+# Python supports several built-in data types:
+    Integers (int): Whole numbers (e.g., 10, -5).
+    Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+    Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+    Booleans (bool): Represents True or False.
+    Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+    Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+    Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+    Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+"""
+# Integers
+
+age = 33
+print(age)
+print(type(age))   # Check the datatype
+
+# Floats
+
+cgpa = 8.6
+print(cgpa)
+print(type(cgpa))   # Check the datatype
+
+# Strings
+
+name = "Tejas"
+print(name)
+print(type(name))   # Check the datatype
+
+# Booleans => value must be first letter is Capital
+
+is_complated = True # is must be False
+print(is_complated)
+print(type(is_complated))   # Check the datatype
