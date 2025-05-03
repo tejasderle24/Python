4a6f33e95523cdc55f845d373a46a04cba9c2a50
tejasderle24 <tejasderle24@gmail.com>
2025-05-03 09:25:46 +0530

    Typecasting

---
+++ 01_Python_Fundamentals/02_Typecasting/01_typecasting.py
@@ -0,0 +1,23 @@
+a = 24
+b = "24"
+d = 245
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# Convert b in Integer
+c= int(b)
+
+print(c)
+print(type(c))
+
+
+print(d)
+print(type(d))
+
+e = str(d)
+print(e)
+print(type(e))
