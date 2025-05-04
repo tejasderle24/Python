5fdb2170a4e8acf5049a5b46ea63243fbcf7b58d
tejasderle24 <tejasderle24@gmail.com>
2025-05-04 09:06:15 +0530

    Loops Quiz

---
+++ 02_Control_Flow_and_Loop/02_Loops/Quiz.py
@@ -0,0 +1,20 @@
+"""
+Qu. Printing the table of 56
+
+Instructions
+Write a Python program that prints the multiplication table of 56 from 1 to 10.
+
+Instructions:
+    Use a loop to print the table.
+    Format the output as shown above.
+    Ensure the program runs without errors.
+"""
+
+for i in range(1,11):
+    print("56 X",i,"=",56*i)
+
+# ORR
+
+
+# for i in range(1,11):
+#     print("56 X",str(i),"=",str(56*i))
