737ad838c585b16e1e26c3d1285c8fdb616510b3
tejasderle24 <tejasderle24@gmail.com>
2025-05-04 09:12:06 +0530

    Conditinal Quiz

---
+++ 02_Control_Flow_and_Loop/01_Conditional/Quiz.py
@@ -0,0 +1,17 @@
+"""
+Understanding Escape Sequence Characters
+
+Instructions
+Escape sequences in Python allow special characters to be represented within strings. Your task is to write a Python program that prints the below text exactly as shown:
+
+    Hello, Python Learner
+    I am "good"
+You are not allowed to use single quotes in your program.
+"""
+
+print("Hello, Python Learner")
+print("I am \"good\"")
+
+# Or
+
+print("Hello, Python Learner\nI am \"good\"")
